74bbbbf3fe4acde586147b8a9929e569cf596ae7
Yevhenii_Dovbnia <alterego.lisa@gmail.com>
2020-05-07 20:08:53 +0300

    +Route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header';
-import Navbar from './Components/Navbar';
-import Profile from './Components/Profile';
+import Header from './Components/Header/Header';
+import Navbar from './Components/Navbar/Navbar';
+import Profile from './Components/Profile/Profile';
+import Dialogs from './Components/Dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
+import News from './Components/News/News';
+import Music from './Components/Music/Music';
+import Settings from './Components/Settings/Settings';
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className='app-wrapper'>
       <Header/>
       <Navbar/>
-      <Profile/>
+      <div className='app-wrapper-content'>
+        <Route path='/Dialogs' component={Dialogs}/>
+        <Route path='/Profile' component={Profile}/>
+        <Route path='/News' component={News}/>
+        <Route path='/Music' component={Music}/>
+        <Route path='/Settings' component={Settings}/>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
